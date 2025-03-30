3e7f4aa2c6ace54e93f5f4fc5059ff9034291045
Roman Leshin <vorzee@gmail.com>
2025-03-30 11:52:38 +0500

    Task 2

--- gh.test.js
+++ gh.test.js
@@ -2,14 +2,17 @@ let page;
 
 beforeEach(async () => {
     page = await browser.newPage();
-    await page.goto("https://github.com/team");
 });
 
 afterEach(() => {
     page.close();
 });
 
-describe("Github page tests", () => {
+describe("Github page tests github.com/team", () => {
+    beforeEach(async () => {
+        await page.goto("https://github.com/team", { timeout: 10000 });
+    });
+
     test("The h1 header content'", async () => {
         const firstLink = await page.$("header div div a");
         await firstLink.click();
@@ -18,14 +21,14 @@ describe("Github page tests", () => {
         expect(title2).toEqual(
             "GitHub · Build and ship software on a single, collaborative platform · GitHub",
         );
-    }, 30000);
+    }, 40000);
 
     test("The first link attribute", async () => {
         const actual = await page.$eval("a", (link) =>
             link.getAttribute("href"),
         );
         expect(actual).toEqual("#start-of-content");
-    }, 15000);
+    }, 25000);
 
     test("The page contains Sign in button", async () => {
         const btnSelector = ".btn-large-mktg.btn-mktg.btn-muted-mktg";
@@ -37,5 +40,43 @@ describe("Github page tests", () => {
             (link) => link.textContent,
         );
         expect(actual).toContain("Sign up for free");
-    }, 20000);
+    }, 30000);
+});
+
+describe("Github page tests github.com", () => {
+    beforeEach(async () => {
+        await page.goto("https://github.com/", { timeout: 10000 });
+    });
+
+    test("Title Main page", async () => {
+        const expectedTitle =
+            "GitHub · Build and ship software on a single, collaborative platform · GitHub";
+
+        await page.waitForSelector("h1", { timeout: 10000 });
+        const title = await page.title();
+        expect(title).toEqual(expectedTitle);
+    }, 25000);
+
+    test("Title copilot", async () => {
+        const expectedTitle =
+            "GitHub Copilot · Your AI pair programmer · GitHub";
+
+        const btnCopilot = await page.$('a[href="/features/copilot"]');
+        await btnCopilot.click();
+        await page.waitForSelector("h1", { timeout: 10000 });
+        const title = await page.title();
+        expect(title).toEqual(expectedTitle);
+    }, 40000);
+
+    test("Title Sign Up", async () => {
+        const expectedTitle = "Sign up to GitHub · GitHub";
+
+        const btnSignUp = await page.$(
+            'button[data-analytics-event*="sign_up"]',
+        );
+        await btnSignUp.click();
+        await page.waitForSelector("h1", { timeout: 10000 });
+        const title = await page.title();
+        expect(title).toEqual(expectedTitle);
+    }, 40000);
 });
